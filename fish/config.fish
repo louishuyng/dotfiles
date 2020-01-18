a34725b57b2ce62842538f9340aeb5997b5cd9f3
nqhuyD <huynguyennbk@gmail.com>
2020-01-18 17:15:17 +0700

    Adding Some Config For Theme and some config for fish

---
+++ fish/config.fish
@@ -0,0 +1,45 @@
+## ALIAS
+alias fuck="git"
+alias dcpd="docker-compose down"
+alias dcpu="docker-compose up"
+alias c="clear"
+alias ez="vi $HOME/.zshrc"
+alias server="python -m SimpleHTTPServer"
+alias socks="ssh -vND 8888 kim"
+alias dpsa="docker ps -a"
+alias dcup="docker-compose up"
+alias dcupd="docker-compose up -d"
+alias brave="open -a 'Brave Browser'"
+alias facebook="open -a 'Brave Browser' http://www.facebook.com "
+alias netflix="open -a 'Brave Browser' http://www.netflix.com "
+alias trello="open -a 'Trello'"
+alias genymotion="open -a 'Genymotion'"
+alias postman="open -a 'Postman'"
+alias skype="open -a 'Skype'"
+alias slack="open -a 'Slack'"
+alias postman="open -a 'Postman'"
+alias chatwork="open -a 'Google Chrome' http://www.chatwork.com "
+alias youtube="open -a 'Google Chrome' http://www.youtube.com "
+alias dk="open -a 'Docker'"
+alias sdn="osascript -e 'tell app \"System Events\" to shut down'"
+alias nv='nvim'
+alias vimrc='nvim ~/.config/nvim/init.vim'
+
+alias tm='tmux'
+alias tmn='tmux new -s XXXX'
+
+alias rf='rm -rf'
+
+alias spotify='/usr/local/lib/node_modules/spotify-cli-mac/./index.js'
+alias sp='/usr/local/lib/node_modules/spotify-cli-mac/./index.js'
+alias gpj='cd ~/DevLife/Project/'
+alias typeracer='cli-typer'
+alias speedTest='speed-test'
+alias ra='ranger'
+
+#Wifi
+alias wfscan='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan'
+alias wfon='networksetup -setairportpower en0 on'
+alias wfoff='networksetup -setairportpower en0 off'
+alias wfjoin='networksetup -setairportnetwork en0'
+alias wfi='networksetup -listallhardwareports'
