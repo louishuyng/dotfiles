d57007a0272d6d0ab36d9d990d818e413659f3d4
Louis <huynguyennbk@gmail.com>
2021-06-06 01:41:04 +0700

    add git signs

--- nvim/lua/gitsigns-nvim.lua
+++ nvim/lua/gitsigns-nvim.lua
@@ -1,11 +1,4 @@
 require("gitsigns").setup {
-    signs = {
-        add = {hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr"},
-        change = {hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr"},
-        delete = {hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr"},
-        topdelete = {hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr"},
-        changedelete = {hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr"}
-    },
     numhl = false,
     keymaps = {
         -- Default keymap options
@@ -14,8 +7,8 @@ require("gitsigns").setup {
         ["n ]h"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
         ["n [h"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''},
         ["n ga"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ["n gu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ["n gr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ["n gA"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ["n gu"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ["n gp"] ='<cmd>lua require"gitsigns".preview_hunk()<CR>',
         ["n gb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
     },
