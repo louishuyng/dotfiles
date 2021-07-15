ab4ee884b969ebb68643581a4c0463355cffbd73
Louis <huynguyennbk@gmail.com>
2021-07-15 13:09:13 +0700

    remove character ident blank line

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -3,7 +3,7 @@ local M = {}
 -- blankline config
 M.blankline = function()
     vim.g.indentLine_enabled = 1
-    vim.g.indent_blankline_char = "▏"
+    vim.g.indent_blankline_char = " "
 
     vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
     vim.g.indent_blankline_buftype_exclude = {"terminal"}
