67546f8e7709e0dc0006f2408d1d18637af3cd21
Louis <huynguyennbk@gmail.com>
2021-08-04 16:58:50 +0700

    update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -51,21 +51,21 @@ M.config = function()
             height = 0.80,
             preview_cutoff = 120
         },
-        file_sorter = require "telescope.sorters".get_fuzzy_file,
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
         file_ignore_patterns = {},
-        generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-        path_display = shorten,
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = {"absolute"},
         winblend = 0,
         border = {},
         borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
         color_devicons = true,
         use_less = true,
         set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
-        file_previewer = require "telescope.previewers".vim_buffer_cat.new,
-        grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
-        qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
         -- Developer configurations: Not meant for general override
-        buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
       },
       extensions = {
           fzf = {
