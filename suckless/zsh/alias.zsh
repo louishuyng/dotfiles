0654b2ac2b060d45103aecc89b3092b603bf3d95
Louis <huynguyennbk@gmail.com>
2021-08-12 20:32:52 +0700

    add some alias for rails

--- suckless/zsh/alias.zsh
+++ suckless/zsh/alias.zsh
@@ -34,9 +34,14 @@ alias postman="open -a 'Postman'"
 alias rnm='rm  -rf node_modules'
 alias npmi='npm install'
 alias rnmai='rm -rf node_modules && yarn'
+
 # RAILS
 alias bdi='bundle install'
 alias bdu='bundle update --bundler'
+alias railst='bundle exec rspec'
+alias railss='bundle exec rails s -p'
+alias railsm='bundle exec rails db:migrate'
+alias railsg='bundle exec rails generate'
 
 # DOCKER
 alias dkcd="docker-compose down"
