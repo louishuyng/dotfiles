6c37946dec3b8a6a8ae154ed92db4c29b6d17d47
Louis <huynguyennbk@gmail.com>
2021-08-16 20:42:09 +0700

    telescope config remove extensions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -51,19 +51,6 @@ M.config = function()
           },
         },
       },
-      extensions = {
-          fzf = {
-              fuzzy = true, -- false will only do exact matching
-              override_generic_sorter = false, -- override the generic sorter
-              override_file_sorter = true, -- override the file sorter
-              case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-              -- the default case_mode is "smart_case"
-          },
-          media_files = {
-              filetypes = {"png", "webp", "jpg", "jpeg"},
-              find_cmd = "rg" -- find command (defaults to `fd`)
-          }
-      }
     }
 end
 
