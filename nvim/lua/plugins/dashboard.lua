40eff506dff79d7ca84268d3c48345bd74cd8fb8
Louis <huynguyennbk@gmail.com>
2021-08-28 15:59:11 +0700

    update dashboard header

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,14 +1,5 @@
 local g = vim.g
 
-g.dashboard_custom_header = {
-":::::::..       ....:::::::::     :. .::::. :::.    :::.:::      .::.",
-";;;;``;;;;   ;;;;;;;;;````;;;     ;;`;.  ,;'`;;;;,  `;;;';;,   ,;;;' ",
-" [[[,/[[['   ''`  `[[.    [[[     [[  [nn[,   [[[[[. '[[ [[  .[[/   ",
-" $$$$$$c    ,,,    `$$    $$'     $$ $\"   $c  $$$ \"Y$c$$  Y$c.$$\"    ",
-" 888b \"88bo,888boood88   o88oo,.__88 Yb,_,8P  888    Y88   Y88P      ",
-" MMMM   \"W\" \"MMMMMMMM\"   \"\"\"\"YUMMMMM  \"YMP\"   MMM     YM    MP       ",
-}
-
 g.dashboard_custom_section = {
     a = {description = {"  Find File                 Ctrl p  "}, command = "Telescope find_files"},
     b = {description = {"  Recents                   SPC  h  "}, command = "Telescope oldfiles"},
