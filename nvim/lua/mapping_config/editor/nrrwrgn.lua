1394da56a2bf87fd41fe6254057eda847c86cbd7
Louis <huynguyennbk@gmail.com>
2021-08-29 23:05:18 +0700

    set mapleader key in lua

--- nvim/lua/mapping_config/editor/nrrwrgn.lua
+++ nvim/lua/mapping_config/editor/nrrwrgn.lua
@@ -2,7 +2,5 @@ local map = require('utils.map').map
 
 local opt = {}
 
-map("n", "<space>nr", ":NR<CR>", opt)
-map("v", "<space>nr", ":NR<CR>", opt)
 map("n", "<space>wr", ":WR!<CR>", opt)
 map("v", "<space>wr", ":WR!<CR>", opt)
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 require("mapping_config.anzu")
 require("mapping_config.buffer")
 require("mapping_config.compe")
