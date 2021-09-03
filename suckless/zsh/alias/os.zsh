d599f39d3a30071512ef4ece5c8b47eb41faf8f6
Louis <huynguyennbk@gmail.com>
2021-09-03 14:06:24 +0700

    update os zsh

---
+++ suckless/zsh/alias/os.zsh
@@ -0,0 +1,30 @@
+# OS
+alias sdn="osascript -e 'tell app \"System Events\" to shut down'"
+alias root='sudo -i'
+alias su='sudo -i'
+
+# reboot / halt / poweroff For Linux
+alias reboot='sudo /sbin/reboot'
+alias poweroff='sudo /sbin/poweroff'
+alias halt='sudo /sbin/halt'
+alias shutdown='sudo /sbin/shutdown'
+
+## pass options to free ##
+alias meminfo='free -m -l -t'
+ 
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+ 
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+## Get server cpu info ##
+alias cpuinfo='lscpu'
+ 
+## older system use /proc/cpuinfo ##
+##alias cpuinfo='less /proc/cpuinfo' ##
+ 
+## get GPU ram on desktop / laptop##
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
