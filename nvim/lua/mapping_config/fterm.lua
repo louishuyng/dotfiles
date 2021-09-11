1477cd2561271da38bd9948494bafe026546fe4c
Louis <huynguyennbk@gmail.com>
2021-09-11 15:40:20 +0700

    fix conflict fterm key

--- nvim/lua/mapping_config/fterm.lua
+++ nvim/lua/mapping_config/fterm.lua
@@ -5,8 +5,8 @@ local opts = {silent = true}
 map('n', '<space>t', ':FloatermToggle<CR>', opts)
 map('t', '<space>t', '<C-\\><C-n>:FloatermToggle<CR>', opts)
 
-map('n', '<space>n', ':FloatermNew<CR>', opts)
-map('t', '<space>n', '<C-\\><C-n>:FloatermNew<CR>', opts)
+map('t', '+', '<C-\\><C-n>:FloatermNew<CR>', opts)
+map('t', '_', '<C-\\><C-n>:FloatermKill<CR>', opts)
 
 map('t', '<Tab>', '<C-\\><C-n>:FloatermNext<CR>', opts)
 map('t', '<S-Tab>', '<C-\\><C-n>:FloatermPrev<CR>', opts)
