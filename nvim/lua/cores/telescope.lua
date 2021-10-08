7b7600ef1c74152ba4ab5dc2afb5ce824e15110c
Louis <huynguyennbk@gmail.com>
2021-10-08 23:48:57 +0700

    telescope update config when fixed scroll

--- nvim/lua/cores/telescope.lua
+++ nvim/lua/cores/telescope.lua
@@ -9,12 +9,10 @@ M.config = function()
       prompt_prefix = "   ",
       selection_caret = " ❯ ",
       entry_prefix = "   ",
-      scroll_strategy = nil,
       sorting_strategy = 'ascending',
       winblend = 0,
       mappings = {
           i = {
-              ["<C-d>"] = actions.close,
               ["<Esc>"] = actions.close,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
@@ -23,7 +21,6 @@ M.config = function()
               ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
           },
           n = {
-              ["<C-d>"] = actions.close,
               ["<Esc>"] = actions.close,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
@@ -33,11 +30,11 @@ M.config = function()
           }
       },
       layout_config = {
-        preview_height = 20,
-        vertical = { width = 0.4, height = 0.7, mirror = true }
+        height = 0.7,
+        width = 0.5,
       },
       initial_mode = "insert",
-      layout_strategy = "vertical",
+      layout_strategy = "flex",
     },
   }
 end
