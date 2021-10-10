df84c5fcdfd433b4c91c56b1d12a74103cbd5250
Louis <huynguyennbk@gmail.com>
2021-10-10 10:09:32 +0700

    seperate config file

---
+++ nvim/config.lua
@@ -0,0 +1,16 @@
+-- Global Config
+require "mappings"
+require "options"
+
+-- Cores Config
+require("cores.git")
+require("cores.sneak")
+
+-- Plugins Config
+require("plugins.dashboard")
+require("plugins.far")
+require("plugins.fterm")
+require("plugins.neoscroll")
+require("plugins.nrrwrgn")
+require("plugins.rangeHighlight")
+require("plugins.wordmotion")
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,6 @@
 require "pluginList"
 
--- Global Config
-require "mappings"
-require "options"
-
--- Cores Config
-require("cores.git")
-require("cores.sneak")
-
--- Plugins Config
-require("plugins.dashboard")
-require("plugins.far")
-require("plugins.fterm")
-require("plugins.neoscroll")
-require("plugins.nrrwrgn")
-require("plugins.rangeHighlight")
-require("plugins.wordmotion")
+require('config')
 
 -- Themes
 require('themes.schema')
