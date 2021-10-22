1ee9ba09d847bf3282ffadad2319562f9f5271c0
Louis <huynguyennbk@gmail.com>
2021-10-22 22:39:26 +0700

    update nvim tree folder icons

--- nvim/lua/cores/nvimtree.lua
+++ nvim/lua/cores/nvimtree.lua
@@ -1,3 +1,4 @@
+local icons = require "nvim-nonicons"
 local present, nvimtree = pcall(require, "nvim-tree")
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
@@ -27,7 +28,17 @@ g.nvim_tree_show_icons = {
 }
 
 g.nvim_tree_icons = {
-  default = "",
+  default = icons.get("file"),
+  folder = {
+    default = icons.get("file-directory"),
+    open = icons.get("file-directory-outline"),
+    symlink = icons.get("file-directory"),
+    symlink_open = icons.get("file-directory-outline"),
+    empty = icons.get("file-directory-outline"),
+    empty_open = icons.get("file-directory-outline"),
+    arrow_open = icons.get("chevron-down"),
+    arrow_closed = icons.get("chevron-right")
+  },
   symlink = "",
   git = {
     deleted = "",
@@ -37,16 +48,6 @@ g.nvim_tree_icons = {
     unmerged = "",
     unstaged = "✗",
     untacked = "★",
-  },
-  folder = {
-    arrow_open = "",
-    arrow_closed = "",
-    default = "",
-    empty = "", -- 
-    empty_open = "",
-    open = "",
-    symlink = "",
-    symlink_open = "",
   },
   lsp = {
     error = " ",
