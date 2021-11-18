3a44683b701fc1bf2c8434f875d75b397782e6dd
Louis <huynguyennbk@gmail.com>
2021-11-18 16:51:17 +0700

    fix: mapping keys for term top and nnn

--- nvim/lua/mappings/telescope.lua
+++ nvim/lua/mappings/telescope.lua
@@ -7,4 +7,3 @@ map("n", "<leader>f", "<cmd>Telescope live_grep layout_config={'width':0.7}<CR>"
 map("n", "<leader>t", "<cmd>Telescope treesitter layout_config={'width':0.7}<CR>", opt, {silent = true})
 map("n", "<leader>b", "<cmd>Telescope buffers previewer=false<CR>", opt, {silent = true})
 map("n", "<leader>o", "<cmd>Telescope oldfiles previewer=false<CR>", opt, {silent = true})
-map("n", "<leader>ht", "<cmd>Telescope help_tags layout_config={'width':0.7}<CR>", opt, {silent = true})
