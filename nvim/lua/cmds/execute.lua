e2a938d86b8b5534488599b6112e6a4ff6092545
Louis <huynguyennbk@gmail.com>
2021-11-24 23:33:10 +0700

    mapping key excecute js file

--- nvim/lua/cmds/execute.lua
+++ nvim/lua/cmds/execute.lua
@@ -1,1 +1,2 @@
 vim.cmd("autocmd BufEnter *.go nnoremap <leader>r :call VimuxRunCommand('clear; go run ' . bufname('%'))<CR>")
+vim.cmd("autocmd BufEnter *.js nnoremap <leader>r :call VimuxRunCommand('clear; node ' . bufname('%'))<CR>")
