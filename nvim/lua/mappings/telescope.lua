95e226af978fdae72c4b7e36f771b235536b6bda
Louis <huynguyennbk@gmail.com>
2021-11-24 09:54:48 +0700

    show only oldfiles telescope in cwd

--- nvim/lua/mappings/telescope.lua
+++ nvim/lua/mappings/telescope.lua
@@ -6,4 +6,4 @@ map("n", "<c-p>", "<cmd>Telescope git_files previewer=false<cr>", opt, {silent =
 map("n", "<leader>f", "<cmd>Telescope live_grep layout_config={'width':0.7}<CR>", opt, {silent = true})
 map("n", "<leader>t", "<cmd>Telescope treesitter layout_config={'width':0.7}<CR>", opt, {silent = true})
 map("n", "<leader>b", "<cmd>Telescope buffers previewer=false<CR>", opt, {silent = true})
-map("n", "<leader>o", "<cmd>Telescope oldfiles previewer=false<CR>", opt, {silent = true})
+map("n", "<leader>h", "<cmd>Telescope oldfiles previewer=false cwd_only=true<CR>", opt, {silent = true})
