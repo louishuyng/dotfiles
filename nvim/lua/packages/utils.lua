c27970503d8a5a49a583bcc250a1bfe9a70aa0e8
Louis <huynguyennbk@gmail.com>
2021-11-29 17:41:33 +0700

    fix: autopairs setting with cmp

--- nvim/lua/packages/utils.lua
+++ nvim/lua/packages/utils.lua
@@ -4,11 +4,18 @@ local use = packer.use
 use {
   "windwp/nvim-autopairs",
   after="nvim-cmp",
-  requires = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local cmp = require('cmp')
-    cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+    local present1, autopairs = pcall(require, "nvim-autopairs")
+    local present2, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+
+    if not (present1 or present2) then
+      return
+    end
+
+    autopairs.setup()
+
+    local cmp = require "cmp"
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
   end
 }
 use 'numToStr/Comment.nvim'
