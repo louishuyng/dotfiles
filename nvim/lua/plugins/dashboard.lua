463162d751c3eef9c3c650fc22a2e48ddd035688
Louis <huynguyennbk@gmail.com>
2022-01-09 14:37:41 +0700

    fix: dashboard function telescope not correct

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,8 +1,8 @@
 local g = vim.g
 
 g.dashboard_custom_section = {
-  a = {description = {"  Find File                 Ctrl p  "}, command = "Telescope find_files"},
-  b = {description = {"  Recents                   SPC  h  "}, command = "Telescope oldfiles"},
-  c = {description = {"  Find Word                 SPC  f  "}, command = "Telescope live_grep"},
-  e = {description = {"  Projects                  SPC  p  "}, command = "Telescope marks"},
+  a = {description = {"  Find File                 Ctrl p  "}, command = "Telescope git_files layout_config={'width':0.7}"},
+  b = {description = {"  Recents                   SPC  h  "}, command = "Telescope oldfiles previewer=false cwd_only=true"},
+  c = {description = {"  Find Word                 SPC  f  "}, command = "Telescope live_grep layout_config={'width':0.7}"},
+  e = {description = {"  Projects                  SPC  p  "}, command = "Telescope project"},
 }
