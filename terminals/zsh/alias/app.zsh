45ae548e95c0d2c6098de1b960185c9f4251a0dc
Louis <huynguyennbk@gmail.com>
2022-01-11 17:41:17 +0700

    alias sourcetree and config sourcetree on yabai

--- terminals/zsh/alias/app.zsh
+++ terminals/zsh/alias/app.zsh
@@ -2,3 +2,4 @@ alias facebook="open -a 'Brave Browser' http://www.facebook.com "
 alias youtube="open -a 'Brave Browser' http://www.youtube.com "
 alias netflix="open -a 'Brave Browser' http://www.netflix.com "
 alias read="zathura "
+alias stree='open -a SourceTree .'
