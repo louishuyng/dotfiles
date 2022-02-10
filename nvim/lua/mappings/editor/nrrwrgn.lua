02208d689ba4e18872813aeffa1533d82d38fd28
Louis <huynguyennbk@gmail.com>
2022-02-11 00:27:39 +0700

    fix: bidning key nrrwrgn for easily to remember

--- nvim/lua/mappings/editor/nrrwrgn.lua
+++ nvim/lua/mappings/editor/nrrwrgn.lua
@@ -2,7 +2,7 @@ local map = require 'utils.map'
 
 local opt = {}
 
-map("n", "<leader>nr", ":NR!<CR>:MaximizerToggle<CR>", opt)
-map("v", "<leader>nr", ":NR!<CR>:MaximizerToggle<CR>", opt)
-map("n", "<leader>wr", ":WR!<CR>", opt)
-map("v", "<leader>wr", ":WR!<CR>", opt)
+map("n", "<leader>no", ":NR!<CR>:MaximizerToggle<CR>", opt)
+map("v", "<leader>no", ":NR!<CR>:MaximizerToggle<CR>", opt)
+map("n", "<leader>nw", ":WR!<CR>", opt)
+map("v", "<leader>nw", ":WR!<CR>", opt)
