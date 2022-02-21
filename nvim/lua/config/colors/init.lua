8325ffc402a70e3b1c377af3961f3e4f55aea3ce
Louis <huynguyennbk@gmail.com>
2022-02-21 11:19:20 +0700

    colors custom inspired by fleet idea

--- nvim/lua/config/colors/init.lua
+++ nvim/lua/config/colors/init.lua
@@ -10,8 +10,15 @@ M.init = function(theme)
    local present, base16 = pcall(require, "base16")
 
    if present then
-      -- first load the base16 theme
-      base16(base16.themes(theme), true)
+      -- first load the base16 custom theme
+      local custom_theme = base16.theme_from_array {
+	"383838"; "404040"; "606060"; "6f6f6f";
+	"808080"; "dcdccc"; "c0c0c0"; "dcdccc";
+	"caa450"; "caa450"; "caa450"; "c35fb2";
+	"dcdccc"; "8579c5"; "55b5a4"; "dcdccc";
+      }
+
+      base16(custom_theme, true)
 
       -- unload to force reload
       package.loaded["config.colors.highlights" or false] = nil
--- nvim/lua/config/cores/git.lua
+++ nvim/lua/config/cores/git.lua
@@ -1,9 +1,9 @@
-vim.g.gitgutter_sign_added = "+"
-vim.g.gitgutter_sign_modified = ">"
-vim.g.gitgutter_sign_removed = "-"
-vim.g.gitgutter_sign_removed_first_line = "^"
-vim.g.gitgutter_sign_removed_above_and_below = "-"
-vim.g.gitgutter_sign_modified_removed = "<"
+vim.g.gitgutter_sign_added = "▌"
+vim.g.gitgutter_sign_modified = "▌"
+vim.g.gitgutter_sign_removed = "▌"
+vim.g.gitgutter_sign_removed_first_line = "▌"
+vim.g.gitgutter_sign_removed_above_and_below = "▌"
+vim.g.gitgutter_sign_modified_removed = "▌"
 
 -- Git Messenger
 vim.g.git_messenger_always_into_popup = true
