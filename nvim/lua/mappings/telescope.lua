23b31a883bf18bda479c29e402722992dec02b3a
Louis <huynguyennbk@gmail.com>
2022-03-22 15:14:02 +0700

    fix: binding keys conflicted

--- nvim/lua/mappings/telescope.lua
+++ nvim/lua/mappings/telescope.lua
@@ -5,6 +5,7 @@ local opt = {}
 map("n", "<c-p>", "<cmd>Telescope find_files hidden=true layout_config={'width':0.7}<cr>", opt, {silent = true})
 map("n", "<leader>f", "<cmd>Telescope live_grep layout_config={'width':0.7}<CR>", opt, {silent = true})
 map("n", "<leader>t", "<cmd>Telescope treesitter layout_config={'width':0.7}<CR>", opt, {silent = true})
+map("n", "<leader>h", "<cmd>Telescope help_tags layout_config={'width':0.7}<CR>", opt, {silent = true})
 map("n", "<leader>o", "<cmd>Telescope oldfiles previewer=false cwd_only=true<CR>", opt, {silent = true})
 map("n", "<leader>p", "<cmd>Telescope project<CR>", opt, {silent = true})
 map("n", "<leader>vrc", "<cmd>Telescope find_files prompt_title=<VimRC> cwd=~/.dotfiles hidden=true<CR>", opt, {silent = true})
