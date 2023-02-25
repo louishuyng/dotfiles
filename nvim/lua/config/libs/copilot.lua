62826cc004aabcfc136bad6b9cafc90122448315
Louis <huynguyennbk@gmail.com>
2023-02-26 02:06:48 +0700

    fix binding key copilot

--- nvim/lua/config/libs/copilot.lua
+++ nvim/lua/config/libs/copilot.lua
@@ -1,4 +1,7 @@
+vim.keymap.set("i", "<C-n>", "<Plug>(copilot-next)")
+vim.keymap.set("i", "<C-p>", "<Plug>(copilot-previous)")
+
 vim.cmd([[
-  imap <silent><script><expr> <C-n> copilot#Accept("\<CR>")
+  imap <silent><script><expr> <Bslash><Bslash> copilot#Accept("\<CR>")
   let g:copilot_no_tab_map = v:true
 ]])
