74cbf078c1bdac6133743d6dd790180afcf8b4d5
Louis <huynguyennbk@gmail.com>
2023-04-02 00:16:45 +0700

    refactor fish prompt

--- terminals/fish/config.fish
+++ terminals/fish/config.fish
@@ -1,43 +1,14 @@
 if status is-interactive
-  set -U fish_greeting ""
+  starship init fish | source
 
   # FIX: set default key bindings fixing https://stackoverflow.com/a/41905020
   set -U fish_key_bindings fish_default_key_bindings
 
   set -U fisher_path ~/.dotfiles/terminals/fish/fisherman
 
-  # Path to oh-my-fish.
-  set -g OMF_PATH $HOME/.local/share/omf
-
-  # Path to oh-my-fish configuration.
-  set -g OMF_CONFIG $HOME/.config/omf
-
-  # Load oh-my-fish configuration.
-  source $OMF_PATH/init.fish
-
-  # FIX: default theme color not behave as expected  https://github.com/fish-shell/fish-shell/issues/3555
-  set fish_pager_color_progress cya
-
   source ~/.dotfiles/terminals/fish/alias/init.fish
   source ~/.dotfiles/terminals/fish/env/init.fish
 
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
   complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-
-# Vi mode cursor change
-  function fish_mode_prompt
-    switch $fish_bind_mode
-      case default
-        echo -en "\e[2 q"
-      case insert
-        echo -en "\e[6 q"
-      case replace_one
-        echo -en "\e[4 q"
-      case visual
-        echo -en "\e[2 q"
-      case '*'
-        echo -en "\e[2 q"
-    end
-    set_color normal
-  end
 end
