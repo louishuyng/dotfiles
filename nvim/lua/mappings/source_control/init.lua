0fcf0acf7e46438e2d0870f9a66ebe7594cda2d3
Louis <huynguyennbk@gmail.com>
2023-04-04 13:46:09 +0700

    new project

--- nvim/lua/mappings/source_control/init.lua
+++ nvim/lua/mappings/source_control/init.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<leader>dc", "::DiffviewClose<CR>")
 vim.keymap.set("n", ",gs", ":Git<CR>:20wincmd_<CR>", {silent = true})
 
 vim.keymap.set("n", "<leader>1",
-               ":silent !ssh-add -D; ssh-add --apple-use-keychain ~/.ssh/id_rsa_oivan <CR><CR>")
+               ":silent !ssh-add -D; ssh-add --apple-use-keychain ~/.ssh/id_rsa_source_code <CR><CR>")
 vim.keymap.set("n", "<leader>2",
                ":silent !ssh-add -D; ssh-add --apple-use-keychain ~/.ssh/id_rsa_open_source <CR><CR>")
 
--- terminals/fish/functions/louisws.fish
+++ terminals/fish/functions/louisws.fish
@@ -1,6 +1,7 @@
 function louisws -d "working space of louis"
   tx start adventure
   tx start perx
+  tx start sourcecode
 
   tm a
 end
