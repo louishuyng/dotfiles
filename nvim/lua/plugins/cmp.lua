8437171072665d04992766408ef5b7f0bb0ca85b
Louis <huynguyennbk@gmail.com>
2023-04-05 23:25:30 +0700

    fix nvim issue with copilot

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -9,5 +9,26 @@ return {
     config = function() require("config.libs.others").luasnip() end
   }, {"saadparwaiz1/cmp_luasnip", dependencies = {"LuaSnip"}},
   {"hrsh7th/cmp-calc", dependencies = {"nvim-cmp"}}, {"hrsh7th/cmp-nvim-lsp"},
-  {"hrsh7th/cmp-buffer", dependencies = {"nvim-cmp"}}, {'github/copilot.vim'}
+  {"hrsh7th/cmp-buffer", dependencies = {"nvim-cmp"}}, {
+    "github/copilot.vim",
+    event = "VeryLazy",
+    build = "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash",
+    config = function()
+      vim.g.copilot_filetypes = {
+        ["chatgpt"] = false,
+        ["python"] = true,
+        ["lua"] = true,
+        ["rust"] = true,
+        ["clojure"] = true,
+        ["R"] = true,
+        ["nu"] = true,
+        ["ruby"] = true,
+        ["javascript"] = true,
+        ["typescript"] = true,
+        ["go"] = true,
+        ["markdown"] = true,
+        ["*"] = false
+      }
+    end
+  }
 }
