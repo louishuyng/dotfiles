7ce3a476358037dbb81c880d8720514b792362d7
Louis <huynguyennbk@gmail.com>
2023-04-06 22:48:05 +0700

    using prettierd for faster format

--- nvim/lua/config/lsp/null_ls.lua
+++ nvim/lua/config/lsp/null_ls.lua
@@ -6,8 +6,10 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.lua_format, null_ls.builtins.formatting.cbfmt,
     null_ls.builtins.formatting.autopep8, null_ls.builtins.formatting.protolint,
+    null_ls.builtins.formatting.prettierd, -- NOTE: prettierd still running as background process so it has to be killed manually
     null_ls.builtins.diagnostics.codespell, null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.rubocop, null_ls.builtins.diagnostics.protolint
+    null_ls.builtins.diagnostics.rubocop,
+    null_ls.builtins.diagnostics.protolint, null_ls.builtins.diagnostics.eslint
   },
   on_attach = function(client, bufnr)
     client.server_capabilities.document_formatting = true
