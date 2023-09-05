aa9ceb8e39d7e950eef326a76326a0378f2ea54d
Louis Huy Nguyen <huynguyennbk@gmail.com>
2023-09-05 23:37:18 +0700

    create create_spec function for rails

--- nvim/lua/mappings/languages/rails.lua
+++ nvim/lua/mappings/languages/rails.lua
@@ -12,4 +12,26 @@ vim.keymap.set("n", "<leader>rt", ":Etask ")
 vim.keymap.set("n", "<leader>rg", ":Generate ")
 
 vim.keymap.set("n", ",r", ":R<CR>")
-vim.keymap.set("n", ",a", ":A<CR>")
+
+function create_spec()
+  local path = vim.fn.expand("%:p")
+
+  -- if path include spec then run :A to open the implementation
+  if string.find(path, "spec") then
+    vim.cmd("A")
+    return
+  end
+
+  -- if no spec_path folder exists, create it
+  local spec_path = string.gsub(path, "app", "spec")
+  if vim.fn.isdirectory(spec_path) == 0 then
+    -- ignore the file at the end of the path
+    local new_dir = string.gsub(spec_path, "/[^/]*$", "")
+    vim.fn.mkdir(new_dir, "p")
+  end
+
+  spec_path = string.gsub(spec_path, ".rb", "_spec.rb")
+  vim.cmd("e " .. spec_path)
+end
+
+vim.keymap.set("n", ",a", ":lua create_spec()<CR>")
