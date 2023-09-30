ab1b2fea26332249799af0aebf740ad946ae4914
Louis Huy Nguyen <huynguyennbk@gmail.com>
2023-09-30 13:26:32 +0700

    Update tmux and using PIA for vpn

--- suckless/mac_os/sketchybar/view/warpvpn.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-sketchybar -m --add item warpvpn left                         \
-              --set warpvpn update_freq=5                    \
-                    label.padding_left=-1                          \
-                    icon.font="$LABEL:Bold:15.0"              \
-                    script="$PLUGIN_DIR/warpvpn.sh"       \
--- terminals/fish/functions/warpvpn.fish
+++
@@ -1,9 +0,0 @@
-function warpvpn -d 'toggle warpvpn'
-  set warp_status (curl -s https://1.1.1.1/cdn-cgi/trace | grep warp=)
-
-  if string match -q -- "*warp=on*" $warp_status
-    warp-cli disconnect
-  else
-    warp-cli connect
-  end
-end
