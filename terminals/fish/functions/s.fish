eec84593e11a23a3cf49e48be22866d9e405d258
Louis Huy Nguyen <huynguyennbk@gmail.com>
2023-10-04 18:41:55 +0700

    using custom rg with tmux

---
+++ terminals/fish/functions/s.fish
@@ -0,0 +1,10 @@
+function s -d 'search using rg'
+  set -l expression $argv[1];
+  set -l path $argv[2];
+
+  if [ -z $path ]
+    rg $expression
+  else
+    rg $expression --glob $path
+  end
+end
