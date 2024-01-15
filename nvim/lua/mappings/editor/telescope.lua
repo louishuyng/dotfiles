e96691a39f33c1bda87a3a9f788fd329b54cf472
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-01-15 21:00:39 +0700

    Fix skhd key bindings open app to avoid conflict with other normal vim combine

--- nvim/lua/mappings/editor/telescope.lua
+++ nvim/lua/mappings/editor/telescope.lua
@@ -1,6 +1,6 @@
 local tmux_session = require('config.cores.telescope.custom.tmux_session')
 
-local opt = {silent = true, noremap = true}
+local opt = { silent = true, noremap = true }
 
 -- Main finding
 vim.keymap.set("n", "<c-p>", ":Telescope find_files hidden=true<cr>", opt)
@@ -14,10 +14,10 @@ vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", opt)
 
 -- prefix with <leader>f
 vim.keymap.set("n", "<leader>f/", ":Telescope current_buffer_fuzzy_find<CR>",
-               opt)
+  opt)
 vim.keymap.set("n", "<leader>fc", ":Telescope flutter commands<CR>", opt)
 vim.keymap.set("n", "<leader>fr",
-               ":Telescope oldfiles previewer=false cwd_only=true<CR>", opt)
+  ":Telescope oldfiles previewer=false cwd_only=true<CR>", opt)
 
 -- searching vim built-in
 vim.keymap.set("n", "g?", ":Telescope help_tags<CR>", opt)
@@ -29,8 +29,8 @@ vim.keymap.set("n", "<leader>\\", tmux_session)
 
 -- vim config
 vim.keymap.set("n", "<leader>vc",
-               ":Telescope find_files prompt_title=<VimRC> cwd=~/.dotfiles hidden=true<CR>",
-               opt)
+  ":Telescope find_files prompt_title=<VimRC> cwd=~/.dotfiles hidden=true<CR>",
+  opt)
 
 -- File Browser with the path of the current path
 vim.keymap.set("n", ",e", function()
@@ -49,7 +49,7 @@ vim.keymap.set("n", ",e", function()
 end, opt)
 
 -- Resume Previous Telescope
-vim.keymap.set("n", ";;", function()
+vim.keymap.set("n", "<leader>fl", function()
   local builtin = require("telescope.builtin")
   builtin.resume()
 end, opt)
@@ -68,10 +68,9 @@ end)
 
 -- Harpoon
 vim.keymap.set("n", "<leader>mf",
-               ":Telescope harpoon marks previewer=false<CR>", opt)
+  ":Telescope harpoon marks previewer=false<CR>", opt)
 
 -- Refactoring
-
-vim.keymap.set({"n", "x"}, "<leader>rf", function()
+vim.keymap.set({ "n", "x" }, "<leader>rf", function()
   require('telescope').extensions.refactoring.refactors()
 end)
