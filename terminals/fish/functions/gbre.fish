9a13facb421303f24b40f7904582b9b7b236f67c
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-03-27 18:08:38 +0700

    ignore k9s config

--- terminals/fish/functions/gbre.fish
+++ terminals/fish/functions/gbre.fish
@@ -2,7 +2,7 @@ function gbre -d 'new git branch regask'
   set -l TYPE "REG"
   set -l TICKET $(gum input --placeholder "Jira Ticket number (If no ticket it will generate XXXX instead)")
 
-  set -l DESCRIPTION $(gum write --placeholder "Short description of the ticket" | tr '[:upper:]' '[:lower:]' | sed 's/ *$//' | tr ' ' '-')
+  set -l DESCRIPTION $(gum input --placeholder "Short description of the ticket" | tr '[:upper:]' '[:lower:]' | sed 's/ *$//' | tr ' ' '-')
 
   # If ticket is empty then set REG-XXXX if not it will be REG-TICKET
   if test -z $TICKET
