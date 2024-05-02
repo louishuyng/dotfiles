2b1d6d98b33310982f5be6592ef50f27f0bf26a3
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-05-02 16:14:56 +0700

    add process clean script

---
+++ terminals/fish/functions/p-clean.fish
@@ -0,0 +1,11 @@
+function p-clean -d 'clean all major processes'
+  set -l array node ruby
+
+  for element in $array
+    if killall $element > /dev/null 2>&1
+      echo "Successfully killed $element"
+    else
+      echo "No running $element processes"
+    end
+  end
+end
