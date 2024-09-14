3dda9bae81ff4619ad353c020d945416f2c6cfeb
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-09-15 01:47:03 +0700

    turn off relative number in neovim

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -20,20 +20,19 @@ opt.wildoptions = "pum"
 opt.termguicolors = true
 opt.showmode = false
 opt.showcmd = true
-opt.cmdheight = 1         -- Height of the command bar
-opt.incsearch = true      -- Makes search act like search in modern browsers
-opt.showmatch = true      -- show matching brackets when text indicator is over them
-opt.relativenumber = true -- Show line numbers
-opt.number = true         -- But show the actual number for the line we're on
-opt.ignorecase = true     -- Ignore case when searching...
-opt.smartcase = true      -- ... unless there is a capital letter in the query
-opt.hidden = true         -- I like having buffers stay around
-opt.splitright = true     -- Prefer windows splitting to the right
-opt.splitbelow = false    -- Prefer windows splitting to the top
-opt.updatetime = 1000     -- Make updates happen faster
-opt.hlsearch = true       -- I wouldn't use this without my DoNoHL function
-opt.scrolloff = 10        -- Make it so there are always ten lines below my cursor
-vim.opt.list = true       -- Show some invisible characters (tabs...)
+opt.cmdheight = 1      -- Height of the command bar
+opt.incsearch = true   -- Makes search act like search in modern browsers
+opt.showmatch = true   -- show matching brackets when text indicator is over them
+opt.number = true      -- But show the actual number for the line we're on
+opt.ignorecase = true  -- Ignore case when searching...
+opt.smartcase = true   -- ... unless there is a capital letter in the query
+opt.hidden = true      -- I like having buffers stay around
+opt.splitright = true  -- Prefer windows splitting to the right
+opt.splitbelow = false -- Prefer windows splitting to the top
+opt.updatetime = 1000  -- Make updates happen faster
+opt.hlsearch = true    -- I wouldn't use this without my DoNoHL function
+opt.scrolloff = 10     -- Make it so there are always ten lines below my cursor
+vim.opt.list = true    -- Show some invisible characters (tabs...)
 
 -- Cursorline highlighting control
 --  Only have it on in the active buffer
