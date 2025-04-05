de2e61f6c74b42081c78568bd345635de6658459
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-04-05 20:17:05 +0700

    new git config and alias

--- terminals/fish/alias/git.fish
+++ terminals/fish/alias/git.fish
@@ -1,24 +1,3 @@
-alias glp='git log --pretty=format:"%C(yellow)%h%Creset - %C(green)%an%Creset, %ar : %s"'
-
-alias gb='git branch'
-alias gco='git checkout'
-
-alias gcm='git commit -m'
-alias gcp='git cherry-pick'
-
-alias gs='git status'
-alias gst='git stash'
-
-alias gp='git push'
-alias gpf='git push --force-with-lease'
-
-alias gm='git merge'
-alias gl='git pull'
-alias glo='git pull origin'
-
-alias grs='git reset --soft'
-alias grh='git reset --hard'
-
 alias ghcs='gh copilot suggest'
 alias ghce='gh copilot explain'
 
