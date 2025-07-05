9083f7604bdbe1227d90c26e400ffdb0be12932a
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-07-05 11:18:17 +0700

    better gitwork tree setup

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -28,6 +28,25 @@ return {
   },
   { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
   { 'akinsho/git-conflict.nvim', version = '*', config = true },
+  {
+    'polarmutex/git-worktree.nvim',
+    version = '^2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local Hooks = require('git-worktree.hooks')
+      local config = require('git-worktree.config')
+      local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+      Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+        vim.notify('Moved from ' .. prev_path .. ' to ' .. path)
+        update_on_switch(path, prev_path)
+      end)
+
+      Hooks.register(Hooks.type.DELETE, function()
+        vim.cmd(config.update_on_change_command)
+      end)
+    end,
+  },
 
   -- Markdown
   {
