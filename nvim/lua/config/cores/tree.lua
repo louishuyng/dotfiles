b6062cd566843ffd300aec23fa4c41f553e54c6e
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-07-20 13:58:43 +0700

    clean neo tree icon

--- nvim/lua/config/cores/tree.lua
+++ nvim/lua/config/cores/tree.lua
@@ -19,7 +19,6 @@ require('neo-tree').setup({
     },
   },
   close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-  popup_border_style = 'rounded',
   enable_git_status = true,
   enable_diagnostics = true,
   open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
@@ -35,20 +34,20 @@ require('neo-tree').setup({
       padding = 1, -- extra padding on left hand side
       -- indent guides
       with_markers = true,
-      indent_marker = '│',
-      last_indent_marker = '└',
+      indent_marker = '╎',
+      last_indent_marker = '╰',
       highlight = 'NeoTreeIndentMarker',
       -- expander config, needed for nesting files
       with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-      expander_collapsed = '',
-      expander_expanded = '',
+      expander_collapsed = '',
+      expander_expanded = '',
       expander_highlight = 'NeoTreeExpander',
     },
     icon = {
-      folder_closed = '',
-      folder_open = '',
-      folder_empty = '󰜌',
-      default = '*',
+      folder_closed = '▶',
+      folder_open = '▼',
+      folder_empty = '󰉖',
+      default = '󰈙',
       highlight = 'NeoTreeFileIcon',
     },
     modified = {
@@ -63,8 +62,8 @@ require('neo-tree').setup({
     git_status = {
       symbols = {
         -- Change type
-        added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+        added = '✚', -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
         deleted = '✖', -- this can only be used in the git_status source
         renamed = '󰁕', -- this can only be used in the git_status source
         -- Status type
