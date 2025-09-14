3196ef748072e51f785ffe014add92b862237442
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-09-14 14:10:23 +0700

    update config

--- suckless/mac_os/sketchybar/view/front_app.sh
+++ suckless/mac_os/sketchybar/view/front_app.sh
@@ -5,7 +5,7 @@ ICON_FONT="sketchybar-app-font"
 sketchybar --add item       front_app left                                    \
            --subscribe      front_app front_app_switched                      \
            --set            front_app script="$PLUGIN_DIR/front_app.sh"       \
-                            icon.font="$ICON_FONT:Regular:16"                 \
+                            icon.font="$ICON_FONT:Regular:20"                 \
                             label.padding_left=2                             \
                             background.padding_left=5 \
                             background.corner_radius=0                \
