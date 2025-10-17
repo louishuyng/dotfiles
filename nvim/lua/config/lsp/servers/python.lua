cd1e180ce0618386f834cd2b590766b954a814e8
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-10-17 11:32:03 +0700

    update python formatter

--- nvim/lua/config/lsp/servers/python.lua
+++ nvim/lua/config/lsp/servers/python.lua
@@ -1,7 +1,28 @@
+vim.lsp.config('ruff', {
+  cmd = { 'ruff-lsp' },
+  filetypes = { 'python' },
+  settings = {
+    organizeImports = true,
+    fixAll = true,
+  },
+})
+
 vim.lsp.config('pyright', {
   cmd = { 'pyright-langserver', '--stdio' },
   filetypes = { 'python' },
-  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
 })
 
+vim.lsp.enable('ruff')
 vim.lsp.enable('pyright')
--- nvim/lua/config/lsp/servers/typescript.lua
+++ nvim/lua/config/lsp/servers/typescript.lua
@@ -38,6 +38,22 @@ vim.lsp.config('vtsls', {
   },
 })
 
+-- Organize imports
+vim.keymap.set('n', '<leader>oi', function()
+  local clients = vim.lsp.get_clients({ bufnr = 0, name = 'vtsls' })
+  if #clients > 0 then
+    -- Since we filtered by name='vtsls', clients[1] is guaranteed to be vtsls
+    local client = clients[1]
+    local params = {
+      command = 'typescript.organizeImports',
+      arguments = { vim.api.nvim_buf_get_name(0) },
+    }
+    client.request('workspace/executeCommand', params, nil, 0)
+  else
+    vim.notify('vtsls client not found', vim.log.levels.WARN)
+  end
+end, { desc = 'Organize Imports' })
+
 vim.lsp.config('eslint', {
   single_file_support = true,
   settings = {
