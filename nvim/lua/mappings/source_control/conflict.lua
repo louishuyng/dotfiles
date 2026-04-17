a17ef5df91c3ddbb88d65db8160185c757dc72eb
Louis Huy Nguyen <huynguyennbk@gmail.com>
2026-04-17 17:18:44 +0700

    fix conflict command run fail

--- nvim/lua/mappings/source_control/conflict.lua
+++ nvim/lua/mappings/source_control/conflict.lua
@@ -1,8 +1,8 @@
-local opts = { silent = true }
+local opts = { silent = true, remap = true }
 
-vim.keymap.set('n', '<leader>cb', ':GitConflictChooseBoth<CR>', opts)
-vim.keymap.set('n', '<leader>ci', ':GitConflictChooseTheirs<CR>', opts)
-vim.keymap.set('n', '<leader>ch', ':GitConflictChooseOurs<CR>', opts)
-vim.keymap.set('n', '<leader>cn', ':GitConflictChooseNone<CR>', opts)
+vim.keymap.set('n', '<leader>cb', '<Plug>(git-conflict-both)', opts)
+vim.keymap.set('n', '<leader>ci', '<Plug>(git-conflict-theirs)', opts)
+vim.keymap.set('n', '<leader>ch', '<Plug>(git-conflict-ours)', opts)
+vim.keymap.set('n', '<leader>cn', '<Plug>(git-conflict-none)', opts)
 
-vim.keymap.set('n', '<leader>co', ':GitConflictListQf<CR>', opts)
+vim.keymap.set('n', '<leader>co', '<Cmd>GitConflictListQf<CR>', { silent = true })
