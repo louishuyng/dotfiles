d80f8f657eb4358ba8e89c49c42029acf4c2098f
Louis Huy Nguyen <huynguyennbk@gmail.com>
2026-04-30 10:39:48 +0700

    refactor buffer key binding

--- nvim/lua/mappings/editor/buffer.lua
+++ nvim/lua/mappings/editor/buffer.lua
@@ -3,7 +3,7 @@ vim.keymap.set('n', '<S-t>', ':tabnew<CR>', {
   desc = 'New Tab',
 })
 
-vim.keymap.set('n', ',bd', ':tabclose<CR>', {
+vim.keymap.set('n', ',tc', ':tabclose<CR>', {
   desc = 'Close Tab',
 })
 
@@ -14,8 +14,12 @@ vim.keymap.set('n', ',q', ':BufferLineCyclePrev<CR>', {
 vim.keymap.set('n', ',w', ':BufferLineCycleNext<CR>', { desc = 'Next Buffer' })
 
 -- Close buffer
-vim.keymap.set('n', ',bda', ':w! <bar> %bd <bar> e# <bar> bd# <CR>', {
-  desc = 'Close all buffer',
+vim.keymap.set('n', ',bd', ':bdelete<CR>', {
+  desc = 'Delete Buffer',
+})
+
+vim.keymap.set('n', ',bD', ':w! <bar> %bd <bar> e# <bar> bd# <CR>', {
+  desc = 'Close all buffers',
 })
 
 --  Alternative Buffer
